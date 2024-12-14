e8c144faf0ebe0997c9e3b437c9eec7e78899584
Abijith-A <aanil6@mycambrian.ca>
2024-12-14 00:16:31 +0000

    Finished the Nuget Package

---
+++ MyClassLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyClassLibrary
+{
+    public class HelloWorld
+    {
+        public void PrintMessage()
+        {
+            Console.WriteLine("Merry Christmas");
+        }
+    }
+}
